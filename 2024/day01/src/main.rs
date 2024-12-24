5403e1a885c3476c509a191d8f4a35eb2c42e2cd
Evan Smith <12199891+e13h@users.noreply.github.com>
2024-12-24 15:04:29 -0800

    Implement merge sort

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,5 +1,6 @@
 use aoc_getter::aoc::get_aoc_puzzle_input;
 use std::result::Result;
+use std::collections::VecDeque;
 
 fn parse_input(puzzle_input: String) -> Result<(Vec<i32>, Vec<i32>), String> {
     let mut col1 = Vec::new();
@@ -37,16 +38,53 @@ fn parse_input(puzzle_input: String) -> Result<(Vec<i32>, Vec<i32>), String> {
     Ok((col1, col2))
 }
 
-// fn merge_sort(input_array: Vec<i32>) -> Vec<i32> {
-//     println!("{:#?}", input_array);
-//     // println!("{}", input_array[0]);
-//     input_array
-// }
+fn merge(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
+    let mut left: VecDeque<i32> = VecDeque::from(left);
+    let mut right: VecDeque<i32> = VecDeque::from(right);
+    let mut result = Vec::new();
+    while (left.len() > 0) & (right.len() > 0) {
+        if left[0] <= right[0] {
+            result.push(left.pop_front().unwrap());
+        } else {
+            result.push(right.pop_front().unwrap());
+        }
+    }
+    let mut left = Vec::from(left);
+    let mut right = Vec::from(right);
+    result.append(&mut left);
+    result.append(&mut right);
+    result
+}
+
+fn merge_sort(input_array: Vec<i32>) -> Vec<i32> {
+    if input_array.len() <= 1 {
+        return input_array;
+    }
+    let left = input_array[..(input_array.len() / 2)].to_vec();
+    let right = input_array[(input_array.len() / 2)..].to_vec();
+    let left = merge_sort(left);
+    let right = merge_sort(right);
+    merge(left, right)
+}
+
+fn verify_sorted(input: Vec<i32>) -> bool {
+    let mut prev = 0;
+    for curr in input {
+        if prev > curr {
+            return false
+        }
+        prev = curr
+    }
+    return true
+}
 
 fn main() {
-    println!("Hello, world!");
     let input = get_aoc_puzzle_input(1).unwrap();
     let (col1, col2) = parse_input(input).unwrap();
-    println!("{:#?}", &col1[0..5]);
-    println!("{:#?}", &col2[0..5]);
+    println!("sorted? {}", verify_sorted(col1.clone()));
+    println!("sorted? {}", verify_sorted(col2.clone()));
+    let col1 = merge_sort(col1);
+    let col2 = merge_sort(col2);
+    println!("sorted? {}", verify_sorted(col1));
+    println!("sorted? {}", verify_sorted(col2));
 }
