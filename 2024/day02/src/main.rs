af6ce244c1c597bf5d60cbe59733b5db99d93479
Evan Smith <12199891+e13h@users.noreply.github.com>
2024-12-28 15:05:01 -0800

    Parse input hello world

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,3 +1,17 @@
+use aoc_getter::aoc::get_aoc_puzzle_input;
+use std::result::Result;
+
+fn parse_input(puzzle_input: String) -> Result<Vec<Vec<i32>>, String> {
+    let mut result = Vec::new();
+    for line in puzzle_input.lines() {
+        let mut report = Vec::new();
+        result.push(report);
+    }
+    Ok(result)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_aoc_puzzle_input(2).unwrap();
+    let reports = parse_input(input).unwrap();
+    println!("Found {} reports", reports.len());
 }
