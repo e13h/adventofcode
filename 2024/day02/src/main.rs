4b068b776a5a23e2f7a6d9d1ecf3a98435b14125
Evan Smith <12199891+e13h@users.noreply.github.com>
2024-12-29 20:38:05 -0800

    Problem 2 part a complete

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -13,8 +13,67 @@ fn parse_input(puzzle_input: String) -> Result<Vec<Vec<i32>>, ParseIntError> {
         .collect()
 }
 
+fn is_strictly_increasing(vec: &[i32], max_step: Option<i32>) -> bool {
+    let max_step = max_step.unwrap_or(i32::MAX);
+    let mut prev = i32::MIN;
+    for (i, value) in vec.into_iter().enumerate() {
+        if i > 0 && (*value <= prev || ((*value - prev).abs() > max_step)) {
+            return false
+        }
+        prev = *value;
+    }
+    return true
+}
+
+fn is_strictly_decreasing(vec: &[i32], max_step: Option<i32>) -> bool {
+    let max_step = max_step.unwrap_or(i32::MAX);
+    let mut prev = i32::MAX;
+    for (i, value) in vec.into_iter().enumerate() {
+        if i > 0 && (*value >= prev || ((*value - prev).abs() > max_step)) {
+            return false
+        }
+        prev = *value;
+    }
+    return true
+}
+
+fn report_is_safe(report: &[i32]) -> bool {
+    is_strictly_increasing(report, Some(3)) || is_strictly_decreasing(report, Some(3))
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_report_is_safe() {
+        // Test cases provided by https://adventofcode.com/2024/day/2
+        assert_eq!(report_is_safe(&[7, 6, 4, 2, 1]), true);
+        assert_eq!(report_is_safe(&[1, 3, 6, 7, 9]), true);
+    }
+    
+    #[test]
+    fn test_invalid_report_is_unsafe() {
+        assert_eq!(report_is_safe(&[1, 2, 7, 8, 9]), false);
+        assert_eq!(report_is_safe(&[9, 7, 6, 2, 1]), false);
+        assert_eq!(report_is_safe(&[1, 3, 2, 4, 5]), false);
+        assert_eq!(report_is_safe(&[8, 6, 4, 4, 1]), false);
+    }
+}
+
+fn count_safe_reports(reports: &Vec<Vec<i32>>) -> i32 {
+    let mut count = 0;
+    for report in reports {
+        if report_is_safe(&report) {
+            count += 1;
+        }
+    }
+    count
+}
+
 fn main() {
     let input = get_aoc_puzzle_input(2).unwrap();
     let reports = parse_input(input).unwrap();
-    println!("Found {} reports", reports.len());
+    println!("{}/{} reports are safe", count_safe_reports(&reports), reports.len())
 }
